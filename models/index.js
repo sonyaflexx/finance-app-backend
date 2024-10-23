4979feccfe9bc84c71337a5d2b86966a5c1c4f44
Alexandr Kalashnikov <159815449+sonyaflexx@users.noreply.github.com>
2024-10-23 11:19:56 +0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -9,20 +9,9 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
-if (process.env.DATABASE_URL) {
-  sequelize = new Sequelize(process.env.DATABASE_URL, config);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST,
-      dialect: config.dialect,
-    }
-  );
-}
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectModule: pg
+});
 
 fs
   .readdirSync(__dirname)
